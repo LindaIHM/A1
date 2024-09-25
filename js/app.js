f8e633d35dd6ee204f96005900ab0f5f11c2ae2f
LindaIHM <lincal524@ihmeducation.se>
2024-09-25 13:52:26 +0200

    Added HTML x CSS

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// Variables
+   // Buttons
+let button0 = document.getElementById("button0");
+
+   // Texts
+let playerRollText = document.getElementById("playerRollText");
+
+   // Data
+let playerRoll = 0;
+
+
+// PROCESSES
+button0.addEventListener("click", function () {
+  getRandomNumberOneToSixForPlayer();
+  showPlayerRollResult();
+});
+
+// CONTROLLERS
+function getRandomNumberOneToSixForPlayer() {
+  playerRoll = Math.floor(Math.random() * 6) + 1;
+}
+
+// VIEWS
+function showPlayerRollResult() {
+  playerRollText.innerHTML = playerRoll;
+}
