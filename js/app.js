ffd8fd2ec4f6f899e7d42f0bc6cf5d0e70f68b2c
LindaIHM <lincal524@ihmeducation.se>
2024-09-26 11:04:34 +0200

    Added attributes for assignment

--- js/app.js
+++ js/app.js
@@ -4,23 +4,47 @@ let button0 = document.getElementById("button0");
 
    // Texts
 let playerRollText = document.getElementById("playerRollText");
+let aiRollText = document.getElementById("aiRollText");
+let result = document.getElementById("result");
+let playerScoreText = document.getElementById("playerScoreText");
+let aiScoreText = document.getElementById("aiScoreText");
 
    // Data
 let playerRoll = 0;
-
+let aiRoll = 0;
+let playerScore = 0;
+let aiScore = 0;
 
 // PROCESSES
 button0.addEventListener("click", function () {
   getRandomNumberOneToSixForPlayer();
   showPlayerRollResult();
+  showAIRollResult();
 });
 
 // CONTROLLERS
 function getRandomNumberOneToSixForPlayer() {
   playerRoll = Math.floor(Math.random() * 6) + 1;
+  aiRoll = Math.floor(Math.random() * 6) + 1;
+}
+for (let i = 0; i < aiRoll; i++){
+  console.log(aiRoll); KOLLA
 }
 
 // VIEWS
 function showPlayerRollResult() {
   playerRollText.innerHTML = playerRoll;
 }
+function showAIRollResult(){
+  aiRollText.innerHTML = aiRoll;
+}
+function showPlayerScore(){
+  playerScoreText.innerHTML = playerScore;
+}
+function showAiScore(){
+  aiScoreText.innerHTML = aiScore;
+}
+function showEvaluation(){
+  result.innerHTML;
+}
+
